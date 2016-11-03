ebab355aa4f9366964e4b273d04b9e36f4717a23
Your Name <you@example.com>
2016-11-03 18:29:30 +0900

    a

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -13,26 +13,35 @@ end
 def callback
   body = request.body.read
 
+  print a
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
+  print b
 
   events = client.parse_events_from(body)
+  print c
   events.each { |event|
+    print d
     case event
     when Line::Bot::Event::Message
+      print e
       case event.type
       when Line::Bot::Event::MessageType::Text
+        print g
         message = {
           type: 'text',
           text: event.message['text']
         }
         client.reply_message(event['replyToken'], message)
+        print h
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        print i
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
         tf.write(response.body)
+        print j
       end
     end
   }
