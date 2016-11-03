08055a5dda0920689f13049ce5d05397bbdd4e46
Your Name <you@example.com>
2016-11-03 18:23:47 +0900

    a

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -13,11 +13,11 @@ end
 def callback
   body = request.body.read
 
-  'signature = request.env['HTTP_X_LINE_SIGNATURE']
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
-'
+
   events = client.parse_events_from(body)
   events.each { |event|
     case event
