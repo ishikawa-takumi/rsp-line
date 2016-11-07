06949665a9d8733fa00b9285214997ae26bd8ed3
Your Name <you@example.com>
2016-11-07 22:25:54 +0900

    a

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -7,8 +7,8 @@ def client
   @client ||= Line::Bot::Client.new { |config|
     #config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     #config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    config.channel_secret = "957226d38fb37308c0f5867fddecd954"
-    config.channel_token = "1Py/SlVGW7cLzr9sZvJeyi3Hrc/8vIxX9cKXqTuA9LmwdFB9wgy4ZluMZ43taYNcUugO+VUcGCygb2MJ53MmiIVh6MW2b1RqFymhA9v7Dn+njyfJWRg344i9v1ceWMQ3Z6fv+4JB6dprS9H2mL2sUwdB04t89/1O/w1cDnyilFU="
+    config.channel_secret = ENV[LINE_CHANNEL_SECRET]
+    config.channel_token = ENV[LINE_CANNEL_TOKEN]
   }
 end
 
