4ea47e3f1c53910729cda5963fc289db7f3b124f
Your Name <you@example.com>
2016-11-21 23:05:49 +0900

    /path/to/callback => /callback

--- lib/webhook_parser.rb
+++ lib/webhook_parser.rb
@@ -3,10 +3,12 @@ module RspApp
     def initialize(app, opts = {})
       @app = app
       @opts = opts
+      p "AAAAAAAAAAAA"
       super
     end
 
     def call(env)
+      p "BBBBBBBBBBBBBb"
       if @opts[:ignore_prefix].nil? or !env['PATH_INFO'].start_with?(@opts[:ignore_prefix])
         super(env)
       else
