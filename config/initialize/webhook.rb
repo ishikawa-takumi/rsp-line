94ca792180fd1cd177002198c5481ef5462cd032
Your Name <you@example.com>
2016-11-21 20:23:25 +0900

    move initializer -> initialize

--- config/initialize/webhook.rb
+++ config/initialize/webhook.rb
@@ -1,3 +1,4 @@
 require File.dirname(__FILE__) + '/../../lib/webhook_parser.rb'
 
+p "now initializer @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 MyApp::Application.config.middleware.swap ActionDispatch::ParamsParser, MyApp::ParamsParser, :ignore_prefix => '/path/to/webhook'
