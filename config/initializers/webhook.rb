750c771974d2122bc7778c37622379e502af3f23
Your Name <you@example.com>
2016-11-21 23:52:34 +0900

    try

--- config/initializers/webhook.rb
+++ config/initializers/webhook.rb
@@ -1,3 +1,3 @@
 require File.dirname(__FILE__) + '/../../lib/webhook_parser.rb'
 
-RspApp::Application.config.middleware.swap ActionDispatch::ParamsParser, RspApp::ParamsParser, :ignore_prefix => '/callback`'
+RspApp::Application.config.middleware.swap ActionDispatch::ParamsParser, RspApp::ParamsParser, :ignore_prefix => '/callback'
